4228603aef3bebb83ac4f2f608c601d8ea5a1325
Frank-Cleasby <frank.cleasby@stu.ocsb.ca>
2025-09-26 18:30:41 -0400

    LED works

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Frank
+ * Created on: Sep 2025
+ * This program displays an LED
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // turns on LED
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+input.onButtonPressed(Button.B, function () {
+    // turns off LED
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
